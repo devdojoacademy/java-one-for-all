53251db06d21bee4c886fe11c48885956eeea000
William Suane <william.suane@devdojo.academy>
2020-11-19 19:42:37 +0100

    source code video 09

--- src/academy/devdojo/javaoneforall/introduction/HelloDevDojo.java
+++ src/academy/devdojo/javaoneforall/introduction/HelloDevDojo.java
@@ -1,6 +1,11 @@
 package academy.devdojo.javaoneforall.introduction;
 
+/**
+ * This class is the HelloDevDojo and this is <b>javadoc</b>
+ */
 public class HelloDevDojo {
+    // I can type anything and it will be completely ignored
+
     public static void main (String[] args){
         System.out.println("KA-ME-HA-ME-HAAA");
     }

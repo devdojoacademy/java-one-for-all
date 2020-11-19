7ca5de8a8288ce71c3671ec813d0d66c6516429c
William Suane <william.suane@devdojo.academy>
2020-11-19 18:55:36 +0100

    project creation

---
+++ src/academy/devdojo/javaoneforall/introduction/HelloDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class HelloDevDojo {
+    public static void main (String[] args){
+        System.out.println("KA-ME-HA-ME-HAAA");
+    }
+}

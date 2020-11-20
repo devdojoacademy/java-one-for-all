c99e201059fb7e0200fb1ce9b15d753b198b1c57
William Suane <william.suane@devdojo.academy>
2020-11-20 19:15:14 +0100

    source code video 11

--- src/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
+++ src/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -4,6 +4,15 @@ public class Class02PrimitiveTypes {
     public static void main(String[] args) {
         // int, double, float, boolean, char, byte, short, long
         int age = 10;
+        long bigNumber = 10000;
+        float salaryFloat = 2500;
+        double salaryDouble = 2500;
+        boolean trueValue = true;
+        boolean falseValue = false;
+        short ageShort = 29;
+        byte ageByte = -128;
+        char character = '\u0041';
         System.out.println("The age is "+age+" years old");
+        System.out.println(character);
     }
 }

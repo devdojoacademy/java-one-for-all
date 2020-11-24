639d42739e437993e55ee241faca435a6fde0865
William Suane <william.suane@devdojo.academy>
2020-11-24 18:00:59 +0100

    source code video 15

---
+++ src/academy/devdojo/javaoneforall/introduction/Class04Operators.java
@@ -0,0 +1,13 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class04Operators {
+    public static void main(String[] args) {
+        // + - / *
+        int number01 = 10;
+        int number02 = 20;
+        double result = number01 / (double) number02;
+
+        System.out.println(result);
+
+    }
+}

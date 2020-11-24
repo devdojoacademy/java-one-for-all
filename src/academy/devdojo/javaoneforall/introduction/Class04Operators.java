68be1fcdbd460532e1aa6c5bb932df27155d8d72
William Suane <william.suane@devdojo.academy>
2020-11-24 18:32:04 +0100

    source code video 1

--- src/academy/devdojo/javaoneforall/introduction/Class04Operators.java
+++ src/academy/devdojo/javaoneforall/introduction/Class04Operators.java
@@ -9,5 +9,22 @@ public class Class04Operators {
 
         System.out.println(result);
 
+        // %
+        int modulo = 21 % 7;
+        System.out.println(modulo);
+
+        // < > <= >= != ==
+        boolean isTenLowerThanTwenty = 10 < 20;
+        boolean isTenGreaterThanTwenty = 10 > 20;
+        boolean isTenLowerOrEqualsThanTwenty = 10 <= 20;
+        boolean isTenGreaterOrEqualsThanTwenty = 10 >= 20;
+        boolean isTenDifferentThanTwenty = 10 != 20;
+        boolean isTenEqualsTwenty = 10 == 10;
+        System.out.println("isTenLowerThanTwenty "+isTenLowerThanTwenty);
+        System.out.println("isTenGreaterThanTwenty "+isTenGreaterThanTwenty);
+        System.out.println("isTenLowerOrEqualsThanTwenty "+isTenLowerOrEqualsThanTwenty);
+        System.out.println("isTenGreaterOrEqualsThanTwenty "+isTenGreaterOrEqualsThanTwenty);
+        System.out.println("isTenDifferentThanTwenty "+isTenDifferentThanTwenty);
+        System.out.println("isTenEqualsTwenty "+isTenEqualsTwenty);
     }
 }

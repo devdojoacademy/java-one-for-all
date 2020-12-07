71aac71181194be4503601b7b9cb02088da696b7
William Suane <william.suane@devdojo.academy>
2020-12-07 18:58:01 +0100

    source code video 31

---
+++ src/academy/devdojo/javaoneforall/introduction/Class06LoopStatements05.java
@@ -0,0 +1,19 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class06LoopStatements05 {
+    /*
+    Given a car's total price, for example 20000
+    I want to find how much and for how long I'll have to pay monthly.
+    Condition: The monthly payment cannot be lower than 1000
+     */
+    public static void main(String[] args) {
+        double totalPrice = 20000;
+        for (int monthCount = (int) totalPrice; monthCount >= 1; monthCount--) {
+            double monthlyPrice = totalPrice / monthCount;
+            if(monthlyPrice < 1000){
+                continue;
+            }
+            System.out.println("Months " + monthCount + " Price: " + monthlyPrice);
+        }
+    }
+}

a96201f12696246880ad550d4c64a7cd4bbf3b08
William Suane <william.suane@devdojo.academy>
2020-12-08 19:22:36 +0100

    source code video 32

---
+++ src/academy/devdojo/javaoneforall/introduction/Class07Arrays01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class07Arrays01 {
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+        ages[0] = 12;
+        ages[1] = 13;
+        ages[2] = 14;
+        System.out.println(ages[2]);
+
+    }
+}

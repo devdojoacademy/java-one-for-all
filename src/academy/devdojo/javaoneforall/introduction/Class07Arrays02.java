554fea61e83d40186108838006dbdb85d22ed12d
William Suane <william.suane@devdojo.academy>
2020-12-08 19:36:25 +0100

    source code video 33

---
+++ src/academy/devdojo/javaoneforall/introduction/Class07Arrays02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class07Arrays02 {
+    public static void main(String[] args) {
+        //byte, short, double, float, long, int  = 0
+        // char '\u0000' ''
+        //boolean false
+        // String null
+        String[] names = new String[3];
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+
+    }
+}

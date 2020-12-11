9ecae6b6d612ce660b79ff984272fe397cf95f1c
William Suane <william.suane@devdojo.academy>
2020-12-11 17:43:17 +0100

    source code video 36

---
+++ src/academy/devdojo/javaoneforall/introduction/Class08MultidimensionalArrays01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class08MultidimensionalArrays01 {
+    public static void main(String[] args) {
+        int[][] days = new int[3][3];
+        days[0][0] = 31;
+        days[0][1] = 28;
+        days[0][2] = 31;
+        days[1][0] = 30;
+        days[1][1] = 31;
+        days[1][2] = 30;
+
+        for (int i = 0; i < days.length; i++) {
+            for (int j = 0; j < days[i].length; j++) {
+                System.out.println(days[i][j]);
+            }
+        }
+    }
+}

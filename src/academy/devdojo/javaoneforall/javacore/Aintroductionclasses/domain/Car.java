9c1ab9208203f5b9bd0e0157689d25c356389f32
William Suane <william.suane@devdojo.academy>
2020-12-16 19:23:16 +0100

    source code video 42

---
+++ src/academy/devdojo/javaoneforall/javacore/Aintroductionclasses/domain/Car.java
@@ -0,0 +1,7 @@
+package academy.devdojo.javaoneforall.javacore.Aintroductionclasses.domain;
+
+public class Car {
+    public String name;
+    public String model;
+    public int year;
+}
---
+++ src/academy/devdojo/javaoneforall/javacore/Aintroductionclasses/test/CarTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.javaoneforall.javacore.Aintroductionclasses.test;
+
+import academy.devdojo.javaoneforall.javacore.Aintroductionclasses.domain.Car;
+
+public class CarTest01 {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        car1.name = "Ferrari";
+        car1.model = "Enzo";
+        car1.year = 2007;
+
+        car2.name = "Mustang";
+        car2.model = "GT 500";
+        car2.year = 1968;
+
+        System.out.println("Car 1");
+
+        System.out.println(car1.name);
+        System.out.println(car1.model);
+        System.out.println(car1.year);
+
+        System.out.println("\nCar 2");
+
+        System.out.println(car2.name);
+        System.out.println(car2.model);
+        System.out.println(car2.year);
+    }
+}

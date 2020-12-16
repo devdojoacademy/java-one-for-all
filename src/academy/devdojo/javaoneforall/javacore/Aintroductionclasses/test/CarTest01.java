e913b90094876b3f05599347704f4ede47b06bb8
William Suane <william.suane@devdojo.academy>
2020-12-16 19:38:54 +0100

    source code video 43

--- src/academy/devdojo/javaoneforall/javacore/Aintroductionclasses/test/CarTest01.java
+++ src/academy/devdojo/javaoneforall/javacore/Aintroductionclasses/test/CarTest01.java
@@ -15,6 +15,8 @@ public class CarTest01 {
         car2.model = "GT 500";
         car2.year = 1968;
 
+        car2 = car1;
+
         System.out.println("Car 1");
 
         System.out.println(car1.name);

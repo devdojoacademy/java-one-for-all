3d77a1d107a64b8173b83d435802fd52ae18c800
William Suane <william.suane@devdojo.academy>
2021-01-04 17:56:11 +0100

    source code video 61

---
+++ src/academy/devdojo/javaoneforall/javacore/Fstaticmodifier/domain/Car.java
@@ -0,0 +1,36 @@
+package academy.devdojo.javaoneforall.javacore.Fstaticmodifier.domain;
+
+public class Car {
+    private String name;
+    private double maxSpeed;
+    public static double speedLimit = 250;
+
+    public Car(String name, double maxSpeed) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void print() {
+        System.out.println("---------------");
+        System.out.println("Name " + this.name);
+        System.out.println("Max speed " + this.maxSpeed);
+        System.out.println("Speed limit " + Car.speedLimit);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+}
---
+++ src/academy/devdojo/javaoneforall/javacore/Fstaticmodifier/test/CarTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.javaoneforall.javacore.Fstaticmodifier.test;
+
+import academy.devdojo.javaoneforall.javacore.Fstaticmodifier.domain.Car;
+
+public class CarTest01 {
+    public static void main(String[] args) {
+        System.out.println(Car.speedLimit);
+
+        Car car1 = new Car("BMW",270);
+        Car car2 = new Car("Mercedes",285);
+        Car car3 = new Car("Audi",290);
+
+
+        car1.print();
+        Car.speedLimit = 180;
+        car1.print();
+        car2.print();
+        car3.print();
+    }
+}

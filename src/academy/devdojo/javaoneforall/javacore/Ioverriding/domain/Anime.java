8e2206cf0927b537358e321eb303e8bebadab3bb
William Suane <william.suane@devdojo.academy>
2021-01-14 19:07:04 +0100

    source code video 76

---
+++ src/academy/devdojo/javaoneforall/javacore/Ioverriding/domain/Anime.java
@@ -0,0 +1,24 @@
+package academy.devdojo.javaoneforall.javacore.Ioverriding.domain;
+
+public class Anime {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Anime{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/academy/devdojo/javaoneforall/javacore/Ioverriding/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javaoneforall.javacore.Ioverriding.test;
+
+import academy.devdojo.javaoneforall.javacore.Ioverriding.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime.toString());
+    }
+}

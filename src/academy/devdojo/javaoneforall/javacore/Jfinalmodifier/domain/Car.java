3aeeaddb1422c782f77e9c8132a527dc8070e900
William Suane <william.suane@devdojo.academy>
2021-01-14 19:36:52 +0100

    source code video 77

---
+++ src/academy/devdojo/javaoneforall/javacore/Jfinalmodifier/domain/Car.java
@@ -0,0 +1,14 @@
+package academy.devdojo.javaoneforall.javacore.Jfinalmodifier.domain;
+
+public class Car {
+    private String name;
+    public static final double SPEED_LIMIT = 250;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/academy/devdojo/javaoneforall/javacore/Jfinalmodifier/test/CarTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javaoneforall.javacore.Jfinalmodifier.test;
+
+import academy.devdojo.javaoneforall.javacore.Jfinalmodifier.domain.Car;
+
+public class CarTest01 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car.SPEED_LIMIT);
+    }
+}

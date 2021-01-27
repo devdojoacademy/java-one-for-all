2b6d92ed6e6dc0f6cd11559b7e0400e6772f265e
William Suane <william.suane@devdojo.academy>
2021-01-27 17:26:09 +0100

    source code video 95

---
+++ src/academy/devdojo/javaoneforall/javacore/Oexception/test/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.javaoneforall.javacore.Oexception.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursion() {
+        recursion();
+    }
+}

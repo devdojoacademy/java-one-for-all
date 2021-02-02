857d1769cdb0968cb92fb8c22b6c3bf0f357a6b8
William Suane <william.suane@devdojo.academy>
2021-02-02 18:14:22 +0100

    source code video 101

---
+++ src/academy/devdojo/javaoneforall/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,33 @@
+package academy.devdojo.javaoneforall.javacore.Oexception.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Inside ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Inside IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Inside IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("Inside ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Inside RuntimeException");
+        }
+
+        try{
+            mayThrowException();
+        }catch (Exception e){
+
+        }
+    }
+
+    private static void mayThrowException() throws SQLException, FileNotFoundException{
+
+    }
+}

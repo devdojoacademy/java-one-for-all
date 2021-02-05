2419ec072dc2f903d4a1c1da74024164bc0b82a2
William Suane <william.suane@devdojo.academy>
2021-02-05 16:20:44 +0100

    source code video 106

---
+++ src/academy/devdojo/javaoneforall/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.javaoneforall.javacore.Pwrapper.test;
+
+import java.util.List;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+
+        
+    }
+}

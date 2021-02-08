e52eb5b92b4158e19dfce0f402296bdacc7dc8ee
William Suane <william.suane@devdojo.academy>
2021-02-08 17:55:46 +0100

    source code video 108

---
+++ src/academy/devdojo/javaoneforall/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.javaoneforall.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String name = "William";
+        String name2 = "William";
+        name = name.concat(" Suane");
+        System.out.println(name);
+        System.out.println(name == name2);
+        String name3 = new String("William2");
+        String name4 = "William2";
+        System.out.println(name3.intern() == name4); // 1 creating reference variable, 2 a String object, 3 Create in the pool of strings
+
+    }
+}

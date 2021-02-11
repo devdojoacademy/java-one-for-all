c3c985a15bb0c80a6a9b0209e100297fcdf051ab
William Suane <william.suane@devdojo.academy>
2021-02-11 18:07:07 +0100

    source code video 112

---
+++ src/academy/devdojo/javaoneforall/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javaoneforall.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}

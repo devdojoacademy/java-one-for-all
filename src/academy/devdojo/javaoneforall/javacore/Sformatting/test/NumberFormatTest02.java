70cb1b9506f04a871051d80744e454a3a7dc8253
William Suane <william.suane@devdojo.academy>
2021-02-15 17:23:48 +0100

    source code video 117

--- src/academy/devdojo/javaoneforall/javacore/Sformatting/test/NumberFormatTest02.java
+++ src/academy/devdojo/javaoneforall/javacore/Sformatting/test/NumberFormatTest02.java
@@ -4,30 +4,29 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
         Locale localeBR = new Locale("pt", "BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
         NumberFormat[] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeJP);
-        nfa[2] = NumberFormat.getInstance(localeBR);
-        nfa[3] = NumberFormat.getInstance(localeIT);
-        double value = 100_000_000.2130;
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
+
+        double value = 1234.1234;
         for (NumberFormat numberFormat : nfa) {
             System.out.println(numberFormat.getMaximumFractionDigits());
-            numberFormat.setMaximumFractionDigits(2);
             System.out.println(numberFormat.format(value));
         }
 
-        String stringValue = "1234,1234";
+        String stringValue = "$1,234.12";
         try {
-            System.out.println(nfa[2].parse(stringValue));
+            System.out.println(nfa[0].parse(stringValue));
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
 
     }
 }

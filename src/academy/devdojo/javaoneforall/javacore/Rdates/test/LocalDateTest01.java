280ef6b8d049b0aeb57bcab004f4dc393717c326
William Suane <william.suane@devdojo.academy>
2021-02-16 18:45:33 +0100

    source code video 119

---
+++ src/academy/devdojo/javaoneforall/javacore/Rdates/test/LocalDateTest01.java
@@ -0,0 +1,32 @@
+package academy.devdojo.javaoneforall.javacore.Rdates.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        System.out.println(Month.JANUARY.getValue());
+        LocalDate date = LocalDate.of(2022, Month.JANUARY, 27);
+        LocalDate now =LocalDate.now();
+        now = now.plusDays(2);
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+        System.out.println(date.getMonthValue());
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.lengthOfMonth());
+        System.out.println(date.isLeapYear());
+        System.out.println(date.get(ChronoField.YEAR));
+        System.out.println(date.get(ChronoField.DAY_OF_MONTH));
+        System.out.println(date);
+        System.out.println(now);
+        System.out.println(LocalDate.MIN);
+        System.out.println(LocalDate.MAX);
+    }
+}

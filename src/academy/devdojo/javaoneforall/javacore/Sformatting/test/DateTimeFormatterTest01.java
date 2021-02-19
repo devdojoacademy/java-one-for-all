f3b13d9385dd630c0b4ce8063d9b68cc9894335a
William Suane <william.suane@devdojo.academy>
2021-02-19 19:07:26 +0100

    source code video 129

---
+++ src/academy/devdojo/javaoneforall/javacore/Sformatting/test/DateTimeFormatterTest01.java
@@ -0,0 +1,41 @@
+package academy.devdojo.javaoneforall.javacore.Sformatting.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateTimeFormatterTest01 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date.format(DateTimeFormatter.ISO_DATE);
+        String s3 = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        LocalDate parse1 = LocalDate.parse("20210219", DateTimeFormatter.BASIC_ISO_DATE);
+        LocalDate parse2 = LocalDate.parse("2021-02-19+05:00", DateTimeFormatter.ISO_DATE);
+        LocalDate parse3 = LocalDate.parse("2021-02-19", DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(parse1);
+        System.out.println(parse2);
+        System.out.println(parse3);
+        LocalDateTime now = LocalDateTime.now();
+        String s4 = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(s4);
+        LocalDateTime parse4 = LocalDateTime.parse("2021-02-19T19:01:48.4109373");
+        System.out.println(parse4);
+        // dd/MM/yyyy, MM/dd/yyyy, yyyy/MM/dd
+        DateTimeFormatter formatterBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formatBR = LocalDate.now().format(formatterBR);
+        System.out.println(formatBR);
+        LocalDate parseBR = LocalDate.parse("19/02/2021", formatterBR);
+        System.out.println(parseBR);
+
+        DateTimeFormatter formatterIT = DateTimeFormatter.ofPattern("dd.MMMM.yyyy", Locale.ITALY);
+        String formatIT = LocalDate.now().format(formatterIT);
+        System.out.println(formatIT);
+        LocalDate parseIT = LocalDate.parse("19.febbraio.2021", formatterIT);
+        System.out.println(parseIT);
+    }
+}

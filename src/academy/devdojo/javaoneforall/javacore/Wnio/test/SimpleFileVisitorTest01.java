f62a1a02a5ed61bc30b80e23d243bc4012facb31
William Suane <william.suane@devdojo.academy>
2021-03-10 17:22:17 +0100

    source code video 154

---
+++ src/academy/devdojo/javaoneforall/javacore/Wnio/test/SimpleFileVisitorTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.javaoneforall.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListAllFiles extends SimpleFileVisitor<Path> {
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        Path fileName = file.getFileName();
+        if (fileName.toString().endsWith(".java")) {
+            System.out.println(fileName);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class SimpleFileVisitorTest01 {
+    public static void main(String[] args) throws IOException {
+        Path root = Paths.get(".");
+        Files.walkFileTree(root, new ListAllFiles());
+    }
+}

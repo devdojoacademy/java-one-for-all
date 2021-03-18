2b18071bed4302c3168a180815ea840eedcd6c8f
William Suane <william.suane@devdojo.academy>
2021-03-18 18:35:18 +0100

    source code video 167

--- src/academy/devdojo/javaoneforall/javacore/Ycollection/test/ListTest01.java
+++ src/academy/devdojo/javaoneforall/javacore/Ycollection/test/ListTest01.java
@@ -6,18 +6,24 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
+        List<String> names2 = new ArrayList<>();
         names.add("William");
-        names.add("DevDojo Academy");
-
-        for(String name : names){
+        names.add("DevDojo");
+        names2.add("Suane");
+        names2.add("Academy");
+//        System.out.println(names.remove("William"));
+        names.addAll(0,names2);
+        for (String name : names) {
             System.out.println(name);
         }
-        names.add("Suane");
-        System.out.println("----------");
 
-        for(int i = 0; i < names.size(); i++){
+        System.out.println("----------");
+        int size = names.size();
+        for (int i = 0; i < size; i++) {
             System.out.println(names.get(i));
         }
+
+        System.out.println(names);
 
     }
 }

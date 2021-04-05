3c131626b116e3816cbfc6918dee5fd189dbb199
William Suane <william.suane@devdojo.academy>
2021-04-05 18:34:31 +0200

    source code video 185

--- src/academy/devdojo/javaoneforall/javacore/Zgenerics/test/WildcardTest02.java
+++ src/academy/devdojo/javaoneforall/javacore/Zgenerics/test/WildcardTest02.java
@@ -9,16 +9,32 @@ public class WildcardTest02 {
         List<Dog> dogs = List.of(new Dog(), new Dog());
         List<Cat> cats = List.of(new Cat(), new Cat());
         List<Animal> animals = new ArrayList<>(2);
-        printConsult(animals);
+        List<Object> objects = new ArrayList<>(2);
+        printConsult(dogs);
+        printConsult(cats);
+        printConsultDog(animals);
     }
 
-    private static void printConsult(List<Animal> animals) {
+    private static void printConsult(List<? extends Animal> animals) {
         System.out.println("-------");
-        animals.add(new Dog());
 
         for (Animal animal : animals) {
             animal.consult();
         }
+    }
 
+    private static void printConsultDog(List<? super Animal> animals) {
+        System.out.println("-------");
+        Animal a = new Cat();
+        Animal a1 = new Dog();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        for (Object obj : animals) {
+            if (obj instanceof Dog) {
+                Dog d = (Dog) obj;
+                d.consult();
+            }
+
+        }
     }
 }

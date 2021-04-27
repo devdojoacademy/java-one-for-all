737a508876e161dc6fb043a86247aabe9ce7efb6
William Suane <william.suane@devdojo.academy>
2021-04-27 17:57:35 +0200

    source code video 220

---
+++ src/academy/devdojo/javaoneforall/javacore/ZZFthreads/test/ThreadsTest01.java
@@ -0,0 +1,57 @@
+package academy.devdojo.javaoneforall.javacore.ZZFthreads.test;
+
+class ThreadExample extends Thread {
+    private final char c;
+
+    public ThreadExample(char c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
+
+class ThreadExampleRunnable implements Runnable {
+    private final char c;
+
+    public ThreadExampleRunnable(char c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
+
+// Daemon x User
+public class ThreadsTest01 {
+    public static void main(String[] args) {
+//        ThreadExample t1 = new ThreadExample('A');
+//        ThreadExample t2 = new ThreadExample('B');
+//        ThreadExample t3 = new ThreadExample('C');
+//        ThreadExample t4 = new ThreadExample('D');
+        Thread t1 = new Thread(new ThreadExampleRunnable('A'));
+        Thread t2 = new Thread(new ThreadExampleRunnable('B'));
+        Thread t3 = new Thread(new ThreadExampleRunnable('C'));
+        Thread t4 = new Thread(new ThreadExampleRunnable('D'));
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}

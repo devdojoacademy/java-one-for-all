f9e48f8166fb6bb5f741937775e0cc309bba2077
William Suane <william.suane@devdojo.academy>
2021-04-30 18:17:03 +0200

    source code video 226

---
+++ src/academy/devdojo/javaoneforall/javacore/ZZFthreads/test/ThreadSafeTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.javaoneforall.javacore.ZZFthreads.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class ThreadSafeNames {
+    private final List<String> names = new ArrayList<>();
+
+    public synchronized void add(String name){
+        names.add(name);
+    }
+    public synchronized void removeFirst(){
+        if(names.size() > 0){
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(names.remove(0));
+        }
+    }
+}
+
+public class ThreadSafeTest01 {
+    public static void main(String[] args) {
+        ThreadSafeNames threadSafeNames = new ThreadSafeNames();
+        threadSafeNames.add("Junkrat");
+        Runnable r = threadSafeNames::removeFirst;
+        new Thread(r).start();
+        new Thread(r).start();
+    }
+}

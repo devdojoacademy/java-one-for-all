ec5ad671e332bebba816483e11b32ec83ca88a80
William Suane <william.suane@devdojo.academy>
2021-05-02 19:00:20 +0200

    source code video 229

---
+++ src/academy/devdojo/javaoneforall/javacore/ZZGconcurrent/test/AtomicIntegerTest01.java
@@ -0,0 +1,38 @@
+package academy.devdojo.javaoneforall.javacore.ZZGconcurrent.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Counter {
+    private int count;
+    private AtomicInteger atomicInteger = new AtomicInteger();
+
+    void increment(){
+        count++;
+        atomicInteger.incrementAndGet();
+    }
+    int getCount() {
+        return count;
+    }
+
+    public AtomicInteger getAtomicInteger() {
+        return atomicInteger;
+    }
+}
+public class AtomicIntegerTest01 {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Runnable r = () -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        };
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(counter.getCount());
+        System.out.println(counter.getAtomicInteger());
+    }
+}

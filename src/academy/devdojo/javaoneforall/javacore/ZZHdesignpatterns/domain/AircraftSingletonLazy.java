909412c3212721d7ed3e589222e7ae15cb3fb1cb
William Suane <william.suane@devdojo.academy>
2021-05-11 17:48:23 +0200

    source code video 249

---
+++ src/academy/devdojo/javaoneforall/javacore/ZZHdesignpatterns/domain/AircraftSingletonLazy.java
@@ -0,0 +1,34 @@
+package academy.devdojo.javaoneforall.javacore.ZZHdesignpatterns.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class AircraftSingletonLazy {
+    private static AircraftSingletonLazy INSTANCE;
+    private final Set<String> availableSeats = new HashSet<>();
+    private final String name;
+
+    {
+        availableSeats.add("1A");
+        availableSeats.add("1B");
+    }
+
+    public static AircraftSingletonLazy getINSTANCE() {
+        if (INSTANCE == null) {
+            synchronized (AircraftSingletonLazy.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new AircraftSingletonLazy("787-900");
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private AircraftSingletonLazy(String name) {
+        this.name = name;
+    }
+
+    public boolean bookSeat(String seat) {
+        return availableSeats.remove(seat);
+    }
+}
---
+++ src/academy/devdojo/javaoneforall/javacore/ZZHdesignpatterns/test/AircraftSingletonLazyTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.javaoneforall.javacore.ZZHdesignpatterns.test;
+
+import academy.devdojo.javaoneforall.javacore.ZZHdesignpatterns.domain.AircraftSingletonLazy;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class AircraftSingletonLazyTest01 {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        bookSeat("1A");
+        bookSeat("1A");
+        System.out.println(AircraftSingletonLazy.getINSTANCE());
+        System.out.println(AircraftSingletonLazy.getINSTANCE());
+        Constructor<AircraftSingletonLazy> constructor = AircraftSingletonLazy.class.getDeclaredConstructor(String.class);
+        constructor.setAccessible(true);
+        AircraftSingletonLazy aircraftSingletonLazy = constructor.newInstance("787-900");
+        System.out.println(aircraftSingletonLazy);
+    }
+
+    static void bookSeat(String seat) {
+        AircraftSingletonLazy aircraft = AircraftSingletonLazy.getINSTANCE();
+        System.out.println(aircraft);
+        System.out.println(aircraft.bookSeat(seat));
+    }
+}

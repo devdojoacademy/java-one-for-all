adc717f820eb77a66346c60d8afd2917457cc893
William Suane <william.suane@devdojo.academy>
2021-05-17 22:23:18 +0200

    source code video 258

--- src/main/java/academy/devdojo/javaoneforall/javacore/ZZIjdbc/domain/Producer.java
+++ src/main/java/academy/devdojo/javaoneforall/javacore/ZZIjdbc/domain/Producer.java
@@ -1,66 +1,11 @@
 package academy.devdojo.javaoneforall.javacore.ZZIjdbc.domain;
 
-import java.util.Objects;
+import lombok.Builder;
+import lombok.Value;
 
-public class Producer {
-    private Integer id;
-    private String name;
-
-    public static final class ProducerBuilder {
-        private Integer id;
-        private String name;
-
-        private ProducerBuilder() {
-        }
-
-        public static ProducerBuilder builder() {
-            return new ProducerBuilder();
-        }
-
-        public ProducerBuilder id(Integer id) {
-            this.id = id;
-            return this;
-        }
-
-        public ProducerBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Producer build() {
-            Producer producer = new Producer();
-            producer.name = this.name;
-            producer.id = this.id;
-            return producer;
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Producer producer = (Producer) o;
-        return id.equals(producer.id) && name.equals(producer.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "Producer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
+@Value
+@Builder
+public final class Producer {
+    private final Integer id;
+    private final String name;
 }
--- src/main/java/academy/devdojo/javaoneforall/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/javaoneforall/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -3,10 +3,12 @@ package academy.devdojo.javaoneforall.javacore.ZZIjdbc.test;
 import academy.devdojo.javaoneforall.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.javaoneforall.javacore.ZZIjdbc.domain.Producer;
 import academy.devdojo.javaoneforall.javacore.ZZIjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
     }
 }

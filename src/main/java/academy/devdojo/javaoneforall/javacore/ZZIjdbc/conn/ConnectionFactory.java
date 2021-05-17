60d404cfc080103d62458c5e15d3a4ab081d5974
William Suane <william.suane@devdojo.academy>
2021-05-17 19:46:06 +0200

    source code video 255

---
+++ src/main/java/academy/devdojo/javaoneforall/javacore/ZZIjdbc/conn/ConnectionFactory.java
@@ -0,0 +1,7 @@
+package academy.devdojo.javaoneforall.javacore.ZZIjdbc.conn;
+
+import java.sql.Connection;
+
+public class ConnectionFactory {
+    // java.sql = Connection, Statement, ResultSet, DriverManager
+}

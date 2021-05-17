2de7c855e47f00b7e40f5e95d6b7bb2cded666d9
William Suane <william.suane@devdojo.academy>
2021-05-17 20:20:20 +0200

    source code video 256

---
+++ src/main/java/academy/devdojo/javaoneforall/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.javaoneforall.javacore.ZZIjdbc.test;
+
+import academy.devdojo.javaoneforall.javacore.ZZIjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest01 {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}

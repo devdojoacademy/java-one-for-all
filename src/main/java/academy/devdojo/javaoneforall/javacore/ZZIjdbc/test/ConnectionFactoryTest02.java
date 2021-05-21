6ff1ea3ce9acd1d3bd0ac9032eb73e23d131324f
William Suane <william.suane@devdojo.academy>
2021-05-21 20:37:46 +0200

    source code video 272

--- src/main/java/academy/devdojo/javaoneforall/javacore/ZZIjdbc/test/ConnectionFactoryTest02.java
+++ src/main/java/academy/devdojo/javaoneforall/javacore/ZZIjdbc/test/ConnectionFactoryTest02.java
@@ -9,7 +9,12 @@ import java.util.List;
 @Log4j2
 public class ConnectionFactoryTest02 {
     public static void main(String[] args) {
-        List<Producer> producers = ProducerServiceRowSet.findByNameJdbcRowSet("madhouse");
+        Producer producerToUpdate = Producer.builder().id(1).name("MAD").build();
+        ProducerServiceRowSet.updateJdbcRowSet(producerToUpdate);
+
+        log.info("-------------------------------");
+
+        List<Producer> producers = ProducerServiceRowSet.findByNameJdbcRowSet("");
         log.info(producers);
     }
 }

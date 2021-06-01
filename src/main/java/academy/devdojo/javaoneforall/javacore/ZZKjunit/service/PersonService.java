02fbd8605fa9a742804ab49d997526c441ea4ef1
William Suane <william.suane@devdojo.academy>
2021-06-01 21:46:52 +0200

    source code video 281

--- src/main/java/academy/devdojo/javaoneforall/javacore/ZZKjunit/service/PersonService.java
+++ src/main/java/academy/devdojo/javaoneforall/javacore/ZZKjunit/service/PersonService.java
@@ -2,12 +2,21 @@ package academy.devdojo.javaoneforall.javacore.ZZKjunit.service;
 
 import academy.devdojo.javaoneforall.javacore.ZZKjunit.domain.Person;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class PersonService {
 
     public boolean isAdult(Person person){
-        Objects.requireNonNull(person, "Person can't be null");
-        return person.getAge() >= 18;
+//        Objects.requireNonNull(person, "Person can't be null");
+        if(person == null) {
+            throw new IllegalArgumentException();
+        }
+        return person.getAge() >= 21;
+    }
+
+    public List<Person> filterRemovingNotAdult(List<Person> personList){
+        return personList.stream().filter(this::isAdult).collect(Collectors.toList());
     }
 }
--- src/test/java/academy/devdojo/javaoneforall/javacore/ZZKjunit/service/PersonServiceTest.java
+++ src/test/java/academy/devdojo/javaoneforall/javacore/ZZKjunit/service/PersonServiceTest.java
@@ -2,18 +2,51 @@ package academy.devdojo.javaoneforall.javacore.ZZKjunit.service;
 
 import academy.devdojo.javaoneforall.javacore.ZZKjunit.domain.Person;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonServiceTest {
+    private Person adult;
+    private Person notAdult;
+    private PersonService personService;
 
+    @BeforeEach
+    public void setUp(){
+        adult = new Person(18);
+        notAdult = new Person(15);
+        personService = new PersonService();
+    }
     @Test
     @DisplayName("A person should not be adult if younger than 18")
     void isAdult_ReturnFalse_WhenAgeIsLowerThan18() {
-        Person person = new Person(15);
-        PersonService personService = new PersonService();
-        Assertions.assertFalse(personService.isAdult(person));
+        Assertions.assertFalse(personService.isAdult(notAdult));
+    }
+
+    @Test
+    @DisplayName("A person should be adult if older or equals 18")
+    void isAdult_ReturnTrue_WhenAgeIsGreaterOrEqualThan18() {
+        Assertions.assertTrue(personService.isAdult(adult));
+    }
+
+    @Test
+    @DisplayName("isAdult throws NullPointerException when Person is Null")
+    void isAdult_ThrowNullPointerException_WhenPersonIsNull() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> personService.isAdult(null), "A person can't be null");
+    }
+
+    @Test
+    @DisplayName("Should remove all non adult people from list")
+    void filterRemovingNotAdult_RemoveNonAdultsFromList_WhenListWithPersonIsPassed() {
+        Person person1 = new Person(17);
+        Person person2 = new Person(18);
+        Person person3 = new Person(21);
+        List<Person> personList = List.of(person1, person2, person3);
+        Assertions.assertEquals(2, personService.filterRemovingNotAdult(personList).size());
     }
 }
